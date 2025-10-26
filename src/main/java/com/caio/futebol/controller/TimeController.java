965b7bdfb6885e323fbbad1c1b86d8cca4575c0a
Caio <caiodesouza.cds@gmail.com>
2025-10-26 20:56:55 -0300

    Criando controller do time

--- src/main/java/com/caio/futebol/controller/TimeController.java
+++ src/main/java/com/caio/futebol/controller/TimeController.java
@@ -1,6 +1,5 @@
 package com.caio.futebol.controller;
 
-import com.caio.futebol.entity.Time;
 import com.caio.futebol.service.TimeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,18 +7,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/")
-public class HomeController {
+@RequestMapping("/times")
+public class TimeController {
 
     private final TimeService timeService;
 
-    public HomeController(TimeService timeService){
+    public TimeController(TimeService timeService){
         this.timeService = timeService;
     }
 
     @GetMapping
-    public String home(){
-        return "index";
+    public ModelAndView index(){
+        var mv = new ModelAndView("time/index");
+        var listaTimes = timeService.findAll();
+        mv.addObject("times", listaTimes);
+        return mv;
     }
 
 }
